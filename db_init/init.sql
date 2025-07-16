6da50fb01ba680cd59ef2e908d1a7f0b9436a56a
Guilherme Oliveira <guidugaich.dev@gmail.com>
2025-07-16 11:35:23 -0300

    update db

--- db_init/init.sql
+++ db_init/init.sql
@@ -1,8 +1,9 @@
 CREATE TABLE payments (
-    id UUID PRIMARY KEY,
+    id BIGSERIAL PRIMARY KEY,
     correlation_id UUID UNIQUE NOT NULL,
     amount INTEGER NOT NULL,
     status VARCHAR(20) NOT NULL DEFAULT 'pending', -- e.g., 'pending', 'processed', 'failed'
     processor VARCHAR(20),      -- e.g., 'default', 'fallback'
     created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW()
+    updated_at TIMESTAMP WITHOUT TIME ZONE
 );  
